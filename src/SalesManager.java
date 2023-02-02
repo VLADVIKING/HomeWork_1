fe2a43200762087fa0bc04f0f4c40709a19e11a4
Vlad Khoteenkov <vla_kho@mail.ru>
2023-02-02 20:16:27 +0300

    Medium calculation added

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -15,4 +15,28 @@ public class SalesManager {
         return max;
     }
 
+    public int min(int[] sales) {
+        int min = 0;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int medium(int[] sales) {
+        int medium = 0;
+        int sum = 0;
+        int j = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > min(sales) && sales[i] < max(sales)) {
+                sum += sales[i];
+                j++;
+            }
+        }
+        medium = sum / j;
+        return medium;
+    }
+
 }
